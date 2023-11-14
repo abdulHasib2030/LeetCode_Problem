4d157a3156d1090a20cd15c0cda76a49fcf6e26b
abdul <abdulhasib2030@gmail.com>
2023-11-14 23:41:59 +0600

    string

---
+++ Goal_Parser_Interpretation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        ans = ""
+        for i in range(len(command)):
+            if command[i] == 'G':
+              ans+= 'G'
+            elif command[i] == '(':
+              if command[i+1] == ')':
+                ans += 'o'
+              else:
+                ans+= 'a'
+                ans+= 'l'
+        return (ans)
+
+            
+    
+    
+        

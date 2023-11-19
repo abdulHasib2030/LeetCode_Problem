927e41dc249d820462ee7258d083cf8f10d9c2e5
abdul <abdulhasib2030@gmail.com>
2023-11-19 15:55:06 +0600

    binary search

---
+++ Koko_Eating_Bananas-875.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_eat(mid):
+            actual_h = 0
+            for pile in piles:
+                actual_h += (pile+mid-1)//mid
+            return actual_h <= h
+
+        left = 0
+        right = max(piles)
+        while 1 < right-left:
+            mid = (left+right) // 2
+            if can_eat(mid):
+              right = mid
+            else:
+              left = mid
+        return right
+        
